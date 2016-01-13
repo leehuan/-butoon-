f618cf3ba5e33c756d95231315b45a096dff4130
Leehn <476850965@qq.com>
2016-01-13 18:17:50 +0800

    update

--- Safe/src/cn/wuyun/safe/Splash.java
+++ Safe/src/cn/wuyun/safe/Splash.java
@@ -1,5 +1,11 @@
 package cn.wuyun.safe;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+
 import cn.wuyun.Utils.PackUtils;
 import android.os.Bundle;
 import android.app.Activity;
@@ -21,16 +27,29 @@ public class Splash extends Activity {
 
 	private void initview() {
 		tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
-		//注意在使用settext的时候一定要给其传递一个值，直接获取会报错
-		tv_splash_version.setText("版本:"+PackUtils.versionCode(this));
-		
-		
+		// 注意在使用settext的时候一定要给其传递一个值，直接获取会报错
+		tv_splash_version.setText("版本:" + PackUtils.versionCode(this));
+
 		update();
 	}
 
 	private void update() {
 		// TODO Auto-generated method stub
-		
+		HttpUtils http = new HttpUtils(3000);
+		http.send(HttpMethod.GET, "xxx", new RequestCallBack<String>() {
+
+			@Override
+			public void onFailure(HttpException arg0, String arg1) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onSuccess(ResponseInfo<String> arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 
 }
