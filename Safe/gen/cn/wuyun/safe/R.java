46cbc75509ba5dfed20cc837e752ecd3f3304998
Leehn <476850965@qq.com>
2016-01-14 10:39:48 +0800

    update

--- Safe/gen/cn/wuyun/safe/R.java
+++ Safe/gen/cn/wuyun/safe/R.java
@@ -29,7 +29,8 @@ public final class R {
         public static final int tv_splash_version=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_splash=0x7f030000;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class menu {
         public static final int splash=0x7f070000;
--- Safe/src/cn/wuyun/safe/Splash.java
+++ Safe/src/cn/wuyun/safe/Splash.java
@@ -1,55 +1,99 @@
 package cn.wuyun.safe;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.exception.HttpException;
 import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
-import cn.wuyun.Utils.PackUtils;
+import cn.wuyun.Utils.PageVersionUtils;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
 public class Splash extends Activity {
 
+	private String UPDATEURL = "http://192.168.3.115:8080/updateinfo.html";
 	private TextView tv_splash_version;
+	private int code;
+	private String apkurl;
+	private String msg;
+	private String json;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-		initview();
-
+		initView();
 	}
 
-	private void initview() {
+	// 锟斤拷锟矫版本
+	private void initView() {
 		tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
-		// 注意在使用settext的时候一定要给其传递一个值，直接获取会报错
-		tv_splash_version.setText("版本:" + PackUtils.versionCode(this));
+		tv_splash_version.setText("锟芥本:" + PageVersionUtils.VersionCode(this));
 
-		update();
+		upDate();
 	}
 
-	private void update() {
+	private void upDate() {
 		// TODO Auto-generated method stub
-		HttpUtils httpUtils = new HttpUtils(3000);
-		httpUtils.send(HttpMethod.GET, "http://192.168.3.115:8080", new RequestCallBack<String>() {
+		HttpUtils http = new HttpUtils(3000);
+
+		http.send(HttpMethod.GET, UPDATEURL, new RequestCallBack<String>() {
 
 			@Override
 			public void onFailure(HttpException arg0, String arg1) {
 				// TODO Auto-generated method stub
-					System.out.println("链接成功");
+				System.out.println("hahaha");
+				skipMain();
 			}
 
 			@Override
-			public void onSuccess(ResponseInfo<String> arg0) {
+			public void onSuccess(ResponseInfo<String> result) {
 				// TODO Auto-generated method stub
-				System.out.println("链接失败");
+				System.out.println("diididid");
+				String json = result.result;
+				prossejson(json);
 			}
 		});
+
+	}
+
+	protected void prossejson(String json) {
+		// TODO Auto-generated method stub
+		try {
+			JSONObject js = new JSONObject(json);
+			code = js.getInt("code");
+			apkurl = js.getString("apkurl");
+			msg = js.getString("msg");
+			if (code == PageVersionUtils.VersionCode(this)) {
+				skipMain();
+			} else {
+
+				Alertbiglog();
+			}
+			System.out.println(code + apkurl + msg);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void skipMain() {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+
+	private void Alertbiglog() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
