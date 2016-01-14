ea5c7cd1fa3f6dfdc39b3ee22314506afe634b13
Leehn <476850965@qq.com>
2016-01-14 12:50:36 +0800

    update

--- Safe/src/cn/wuyun/safe/Splash.java
+++ Safe/src/cn/wuyun/safe/Splash.java
@@ -1,5 +1,7 @@
 package cn.wuyun.safe;
 
+import java.io.File;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,21 +12,34 @@ import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
 import cn.wuyun.Utils.PageVersionUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Splash extends Activity {
 
-	private String UPDATEURL = "http://192.168.3.115:8080/updateinfo.html";
+	private String UPDATEURL = "http://192.168.3.115:8080/update.html";
+	private static final int INSTALLREQUESTCODE=100; 
 	private TextView tv_splash_version;
 	private int code;
 	private String apkurl;
 	private String msg;
 	private String json;
+	private HttpUtils http;
+	private ProgressDialog diglog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +51,20 @@ public class Splash extends Activity {
 	// ���ð汾
 	private void initView() {
 		tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
-		tv_splash_version.setText("�汾:" + PageVersionUtils.VersionCode(this));
-
+		tv_splash_version.setText("版本:" + PageVersionUtils.VersionCode(this));
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				upDate();
+			}
+		}, 3000);
 		upDate();
 	}
 
 	private void upDate() {
-		// TODO Auto-generated method stub
-		HttpUtils http = new HttpUtils(3000);
+		http = new HttpUtils(3000);
 
 		http.send(HttpMethod.GET, UPDATEURL, new RequestCallBack<String>() {
 
@@ -62,7 +83,6 @@ public class Splash extends Activity {
 				prossejson(json);
 			}
 		});
-
 	}
 
 	protected void prossejson(String json) {
@@ -77,8 +97,8 @@ public class Splash extends Activity {
 			} else {
 
 				Alertbiglog();
+
 			}
-			System.out.println(code + apkurl + msg);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -89,11 +109,111 @@ public class Splash extends Activity {
 		// TODO Auto-generated method stub
 		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
+		finish();
 	}
 
 	private void Alertbiglog() {
 		// TODO Auto-generated method stub
+		AlertDialog.Builder alert = new Builder(Splash.this);
+
+		alert.setTitle("更新版本为:" + code);
+		alert.setIcon(R.drawable.ic_launcher);
+		alert.setMessage(msg);
+
+		alert.setOnCancelListener(new OnCancelListener() {
+
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				// TODO Auto-generated method stub
+				skipMain();
+			}
+		});
+
+		alert.setNegativeButton("更新", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				updateDownload();
+			}
+		});
+		alert.setPositiveButton("取消", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				skipMain();
+			}
+		});
+		alert.show();
+	}
+
+	protected void updateDownload() {
+		// TODO Auto-generated method stub
+		if (Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+
+			diglog = new ProgressDialog(this);
+			diglog.setCancelable(false);
+			diglog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			diglog.show();
+
+			http.download(apkurl, "/mnt/sdcard/safe.apk",
+					new RequestCallBack() {
+
+						@Override
+						public void onFailure(HttpException arg0, String arg1) {
+							// TODO Auto-generated method stub
+							skipMain();
+						}
+
+						@Override
+						public void onSuccess(ResponseInfo arg0) {
+							// TODO Auto-generated method stub
+							diglog.dismiss();
+							installAPK();
+						}
+
+						@Override
+						public void onLoading(long total, long current,
+								boolean isUploading) {
+							// TODO Auto-generated method stub
+							super.onLoading(total, current, isUploading);
+							diglog.setMax((int) total);
+							diglog.setProgress((int) current);
+
+						}
+
+					});
+		} else {
+
+			Toast.makeText(this, "SD卡未挂载", 0).show();
+		}
 
 	}
 
+	protected void installAPK() {
+		// TODO Auto-generated method stub
+		/**
+		 * <intent-filter> <action android:name="android.intent.action.VIEW" />
+		 * <category android:name="android.intent.category.DEFAULT" /> <data
+		 * android:scheme="content" /> <data android:scheme="file" /> <data
+		 * android:mimeType="application/vnd.android.package-archive" />
+		 * </intent-filter>
+		 */
+		Intent intent = new Intent();
+		intent.setAction("android.intent.action.VIEW");
+		intent.addCategory("android.intent.category.DEFAULT");
+		intent.setDataAndType(Uri.fromFile(new File("/mnt/sdcard/safe.apk")),
+				"application/vnd.android.package-archive");
+
+		startActivityForResult(intent, INSTALLREQUESTCODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		skipMain();
+	}
 }
