4c37cc0bf0af1d65f3fdc03eb3ff512d8e88bcb8
Leehn <476850965@qq.com>
2016-02-18 16:42:19 +0800

    update

--- MyApplication4/test/src/main/java/com/wunyun/myapplication/MainActivity.java
+++ MyApplication4/test/src/main/java/com/wunyun/myapplication/MainActivity.java
@@ -7,11 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewParent viewById;
     private ViewPager viewById1;
+    private int[] imageResIds = {
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+    };
+
+    private String[] descs = {
+            "巩俐不低俗，我就不能低俗",
+            "扑树又回来啦！再唱经典老歌引万人大合唱",
+            "揭秘北京电影如何升级",
+            "乐视网TV版大派送",
+            "热血屌丝的反杀",
+    };
+
+    private ImageView[] imageViews = new ImageView[imageResIds.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +41,21 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         viewById1 = (ViewPager) findViewById(R.id.view_pager);
         viewById1.setAdapter(pagerAdapter);
+        for (int i = 0; i < imageResIds.length; i++) {
+            setImager(i);
+        }
+    }
+
+    private void setImager(int i) {
+        imageViews[i] = new ImageView(this);
+        imageViews[i].setBackgroundResource(imageResIds[i]);
 
     }
 
     PagerAdapter pagerAdapter = new PagerAdapter() {
         @Override
         public int getCount() {
-            return 0;
+            return imageResIds.length;
         }
 
         @Override
@@ -39,7 +65,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            return super.instantiateItem(container, position);
+            ImageView imageView = imageViews[position];
+            //ViewGroup 将获取的每一个图片添加到ViewGroup中
+            container.addView(imageView);
+
+
+            return imageView;
         }
 
         @Override
